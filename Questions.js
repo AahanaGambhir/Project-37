5bae0b62831438084961e2b312f5368098e17ca4
AahanaGambhir <67320006+AahanaGambhir@users.noreply.github.com>
2021-02-17 23:18:36 +0530

    Add files via upload

---
+++ Questions.js
@@ -0,0 +1,54 @@
+class Question{
+    constructor() {
+        this.title = createElement('h1')
+        this.question = createElement('h2')
+        this.option1 = createElement('h2')
+        this.option2 = createElement('h2')
+        this.input1 = createInput("Your Name")
+        this.input2 = createInput("Answer")
+        this.button = createButton("Submit")
+        this.answer = createElement("h1")
+    }
+
+    hide() {
+        this.title.hide()
+        this.question.hide()
+        this.option1.hide()
+        this.option2.hide()
+        this.input1.hide()
+        this.input2.hide()
+        this.button.hide()
+    }
+
+    display() {
+        this.title.html("Quick Quiz")
+        this.title.position(753, 100)
+
+        this.question.html("Who is the richest person in the world as of today ?")
+        this.question.position(100, 200)
+
+        this.option1.html("1.) Jeff Bezos")
+        this.option1.position(100, 280)
+
+        this.option2.html("2.) Elon Musk")
+        this.option2.position(100, 350)
+
+        this.input1.position(300, 450)
+        this.input2.position(500, 450)
+
+        this.button.position(600, 500)
+
+        this.button.mousePressed(()=>{
+            this.title.hide()
+            this.question.hide()
+            this.option1.hide()
+            this.option2.hide()
+            this.input1.hide()
+            this.input2.hide()
+            this.button.hide()
+            this.answer.html("The Answer Is Jeff Bezos")
+            this.answer.position(600, 200)
+        })
+        
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,17 @@
+var gameState = 0
+var contestantCount, database, quiz, question, contestant
+
+function setup(){
+  canvas = createCanvas(1505,750);
+  database = firebase.database();
+  quiz = new Quiz()
+  quiz.getState()
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+
+  
+}
